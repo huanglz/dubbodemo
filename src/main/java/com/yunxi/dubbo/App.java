9053ce93b75b33ea2f410001a04a5fb5804146ef
huanglz <huang.lingzhuang@dtyunxi.com>
2016-09-05 23:52:08 +0800

    初始化工程

---
+++ src/main/java/com/yunxi/dubbo/App.java
@@ -0,0 +1,13 @@
+package com.yunxi.dubbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
